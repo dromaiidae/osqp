af2c18956f62dc043c30dfcce173a2d760bd0613
Kaushik Kasi <ksk@berkeley.edu>
2018-12-03 15:11:12 -0800

    unoptimized cholesky

--- cs294Solver/example/scratchpadExec.c
+++ cs294Solver/example/scratchpadExec.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
+// Idea: we can allocate a 1-D array and index properly to see if it improves locality
 void *allocArray(int rows, int cols) {
     return malloc(sizeof(double[rows][cols])); // allocate 1 2D-array
+}
+
+void cholesky_unoptimized(int rows, int cols, double array[rows][cols], double decomp[rows][cols]) {
+    int i, j;
+    int sum, sumIndex;
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j <= i; j++) {
+            sum = 0;
+            sumIndex = 0;
+            if (i != j) {
+                for (sumIndex = 0; sumIndex < j; sumIndex++) {
+                    sum += decomp[i][sumIndex]*decomp[j][sumIndex];
+                }
+                double divisor = (1 / decomp[j][j]);
+                decomp[i][j] = divisor * (array[i][j] - sum);
+            } else {
+                for (sumIndex = 0; sumIndex < j; sumIndex++) {
+                    sum += (decomp[j][sumIndex]*decomp[j][sumIndex]);
+                }
+                decomp[i][j] = sqrt(array[j][j] - sum);
+            }
+        }
+    }
 }
 
 int readArray(int rows, int cols, double array[rows][cols], const char *filepath) {
@@ -23,7 +48,7 @@ int main(int argc, char **argv) {
     }
     char *filepath;
     if (argc == 1) {
-        filepath = "data/matrix.mat";
+        filepath = "data/matrix2.mat";
     } else {
         filepath = argv[1];
     }
@@ -31,21 +56,25 @@ int main(int argc, char **argv) {
     int cols = 450;
     int rows = 450;
     double (*matrix)[cols] = allocArray(rows, cols);
+    double (*decomp_cholesky)[cols] = allocArray(rows, cols);
 
     if (readArray(rows, cols, matrix, filepath) != 0) {
         return 1;
     }
 
-    int n = 20;
-    int m = 20;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            printf("%f\t", matrix[i][j]);
+    cholesky_unoptimized(rows, cols, matrix, decomp_cholesky);
+
+    int m = 450;
+    int n = 450;
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%f ", decomp_cholesky[i][j]);
         }
         printf("\n");
     }
 
     free(matrix);
+    free(decomp_cholesky);
     return 0;
 }
 
