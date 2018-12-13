544adce7d153602e202237d1666d838e4366f631
Kaushik Kasi <kaushik.kasi@gmail.com>
2018-12-13 14:00:34 -0800

    timing scripts

--- cs294Solver/example/time_python_decomp.py
+++ cs294Solver/example/time_python_decomp.py
@@ -1,14 +1,15 @@
 import numpy as np
 import datetime
 
-FILENAME = "../data/matrix_4k.mat"
-DIMENSION = 4000
+FILENAME = "../data/matrix2.mat"
+DIMENSION = 450
 
 dense_array_to_decomp = np.fromfile(FILENAME)
 array = np.reshape(dense_array_to_decomp, (DIMENSION, DIMENSION))
 
-start = datetime.datetime.now()
-np.linalg.cholesky(array)
-end = datetime.datetime.now()
+for i in range(5):
+    start = datetime.datetime.now()
+    np.linalg.cholesky(array)
+    end = datetime.datetime.now()
 
-print("Nanoseconds elapsed matrix2:", end.microsecond - start.microsecond)
+    print("Nanoseconds 2 elapsed matrix:", end.microsecond - start.microsecond)
--- cs294Solver/example/time_sparse.py
+++ cs294Solver/example/time_sparse.py
@@ -3,8 +3,8 @@ import datetime
 from sksparse.cholmod import cholesky
 from scipy.sparse import csc_matrix
 
-FILENAME = "../data/matrix.mat"
-DIMENSION = 450
+FILENAME = "../data/matrix_160.mat"
+DIMENSION = 160
 
 dense_array_to_decomp = np.fromfile(FILENAME)
 array = csc_matrix(np.reshape(dense_array_to_decomp, (DIMENSION, DIMENSION)))
